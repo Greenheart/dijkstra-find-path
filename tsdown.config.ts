8f740b3ae45ec285c051bda6150e251312a89cf3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-07 11:50:00 +0100

    Reorder for readability

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  entry: "src/dijkstra.ts",
+  define: {
+    // Remove inline tests when building the library
+    'import.meta.vitest': 'undefined',
+  },
   exports: true,
   dts: {
     "compilerOptions": {
@@ -10,10 +15,5 @@ export default defineConfig({
   },
   attw: {
     profile: "esm-only"
-  },
-  entry: "src/dijkstra.ts",
-  define: {
-    // Remove inline tests when building the library
-    'import.meta.vitest': 'undefined',
   },
 })
