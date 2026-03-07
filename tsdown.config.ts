11705aac07ccfbd2c5e99eaa86175d4ceeede12d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-07 11:43:50 +0100

    Enable checking of the types of the public API

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   exports: true,
   dts: true,
+  attw: {
+    profile: "esm-only"
+  },
   entry: "src/dijkstra.ts",
   define: {
     // Remove inline tests when building the library
